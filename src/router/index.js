0345898ee6a38c1d9cb8f15da8da712eeb11d3cd
Zakamsky <alex.zakamsky@gmail.com>
2020-04-08 01:59:21 +0200

    second part completed

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,52 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'Register',
+    meta: {layout: 'auth'},
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {layout: 'auth'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    meta: {layout: 'main'},
+    component: () => import('../views/Detail.vue')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'Planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
   }
 ]
 
