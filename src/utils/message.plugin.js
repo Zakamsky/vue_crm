638f7fe24616a4127a97daa57f5319ebb9793e57
Zakamsky <alex.zakamsky@gmail.com>
2020-05-18 23:31:02 +0200

    registration and error messages has been realized

--- src/utils/message.plugin.js
+++ src/utils/message.plugin.js
@@ -5,7 +5,7 @@ export default {
     };
 
     Vue.prototype.$error = function(html) {
-      M.toast({html: `[Ошибка]: ${html}`, classes: 'red darken-3'})
+      M.toast({html: `[Ошибка]: ${html}`, classes: 'red-text darken-3'})
     }
   }
 }
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-  'logout': 'Вы вышли из системы'
+  'logout': 'Вы вышли из системы',
+  'auth/user-not-found': 'Пользователя с такими данными не существует.',
+  'auth/wrong-password': 'Пользователя с такими данными не существует.',
+  // 'auth/email-already-in-use': 'Этот email уже занят'
 }
