8ea0d55ed9678af202257b76ca6ce030e2523f7e
Zakamsky <alex.zakamsky@gmail.com>
2020-05-21 18:33:57 +0200

    block category edit was realized

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,7 +4,8 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
+      <Loader v-if="loading"/>
+      <div class="row" v-else>
         <div class="col s12 m6">
 
           <CategoryCreate @created="addNewCategory"/>
@@ -12,7 +13,13 @@
         </div>
         <div class="col s12 m6">
 
-          <CategoryEdit />
+          <CategoryEdit
+            v-if="categories.length"
+            :categories="categories"
+            :key="categories.length + updateCount"
+            @updated="updateCategories"
+          />
+          <p v-else class="orange-text text-darken-4">У вас пока нет ни одной категории, попробуйте создать несколько</p>
 
         </div>
       </div>
@@ -22,19 +29,30 @@
 
 <script>
   import CategoryCreate from "@/components/CategoryCreate"
-  // import CategoryCreate from ""
   import CategoryEdit from "@/components/CategoryEdit"
 
   export default {
     name: "Categories",
     data: () => ({
-      categories: []
+      categories: [],
+      loading: true,
+      updateCount: 0
     }),
+    async mounted() {
+      this.categories = await this.$store.dispatch('fetchCategories')
+      this.loading = false
+    },
     components: {CategoryCreate, CategoryEdit},
     methods: {
       addNewCategory(category) {
         this.categories.push(category)
         console.log(this.categories)
+      },
+      updateCategories(category) {
+        const idx = this.categories.findIndex(c => c.id === category.id)
+        this.categories[idx].title = category.title
+        this.categories[idx].limit = category.limit
+        this.updateCount++
       }
     }
   }
