47f8f5b26cd22d2f762d52fc953d56369d71d56e
Zakamsky <alex.zakamsky@gmail.com>
2020-06-26 20:48:23 +0200

    added chart diagram

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -44,11 +44,13 @@
 </template>
 
 <script>
-  import paginationMixin from '@/mixins/pagination.mixin'
+  import paginationMixin from '@/mixins/pagination.mixin';
   import HistoryTable from "@/components/HistoryTable";
+  import { Pie } from 'vue-chartjs';
 
     export default {
       name: "History",
+      extends: Pie,
       mixins: [paginationMixin],
       data: () => ({
         loading: true,
@@ -57,19 +59,76 @@
       }),
       async mounted() {
         this.records = await this.$store.dispatch('fetchRecords')
-        // const records = await this.$store.dispatch('fetchRecords')
         const categories = await this.$store.dispatch('fetchCategories')
 
-        this.setupPaginations(this.records.map(record => {
-          return{
-            ...record,
-            categoryName: categories.find(c => c.id === record.categoryId).title,
-            typeClass: record.type === 'income' ? 'green' : 'red',
-            typeText: record.type === 'income' ? 'Доход' : 'Расход',
-          }
-        }), this.pageSize)
+        this.setup(categories)
 
         this.loading = false
+      },
+      methods: {
+        setup(categories){
+          this.setupPaginations(this.records.map(record => {
+            return{
+              ...record,
+              categoryName: categories.find(c => c.id === record.categoryId).title,
+              typeClass: record.type === 'income' ? 'green' : 'red',
+              typeText: record.type === 'income' ? 'Доход' : 'Расход',
+            }
+          }), this.pageSize)
+
+          this.renderChart({
+            labels: categories.map(c => c.title),
+            datasets: [{
+              // label: 'Расходы по категориям',
+              label: '# of Votes',
+              data: categories.map(c => {
+                return this.records.reduce((total, r) =>{
+                  if (r.categoryId === c.id && r.type === 'outcome') {
+                    total += +r.amount
+                  }
+                  return total
+                }, 0)
+              }),
+              backgroundColor: [
+                '#ff8a80',
+                '#ff80ab',
+                '#ea80fc',
+                '#b388ff',
+                '#8c9eff',
+                '#82b1ff',
+                '#80d8ff',
+                '#84ffff',
+                '#a7ffeb',
+                '#b9f6ca',
+                '#ccff90',
+                '#f4ff81',
+                '#ffff8d',
+                '#ffe57f',
+                '#ffd180',
+                '#ff9e80'
+              ],
+              borderColor: [
+                '#ff5252',
+                '#ff4081',
+                '#e040fb',
+                '#7c4dff',
+                '#536dfe',
+                '#448aff',
+                '#40c4ff',
+                '#18ffff',
+                '#64ffda',
+                '#69f0ae',
+                '#b2ff59',
+                '#eeff41',
+                '#ffff00',
+                '#ffd740',
+                '#ffab40',
+                '#ff6e40'
+              ],
+              borderWidth: 1
+            }]
+          })
+        }
       },
       components: {
         HistoryTable
@@ -77,8 +136,7 @@
       watch: {
         '$route'(to, from){
           if (!to.query.page){
-            // кликнули в сайдбаре на История
-            this.pageChangeHandler(1);
+            this.pageChangeHandler(1, this.pageSize);
             this.page = 1;
           }
         }
